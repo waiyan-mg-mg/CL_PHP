5316cc68dd5b7783d4ac7b5280fd33e349ee86c6
Matrix Jak <usergit436@gmail.com>
2023-08-17 06:50:34 +0630

    array functions

--- index.php
+++ index.php
@@ -9,8 +9,60 @@
 
     <body>
         <?php
+    // Array function from code lab
+    $data = [
+        [
+            "name" => "Odessa Hood",
+            "list" => "17",
+            "email" => "lobortis.mauris.suspendisse@protonmail.couk"
+        ],
+        [
+            "name" => "Venus Bolton",
+            "list" => "13",
+            "email" => "ornare.sagittis@protonmail.edu"
+        ],
+        [
+            "name" => "Deacon Valencia",
+            "list" => "5",
+            "email" => "tincidunt.dui@aol.org"
+        ],
+        [
+            "name" => "Jaden Foreman",
+            "list" => "7",
+            "email" => "rutrum@hotmail.net"
+        ],
+        [
+            "name" => "Bradley Mcclain",
+            "list" => "5",
+            "email" => "justo.sit@aol.couk"
+        ]
+    ]; // each array contain name , list ,email
+    echo "<pre></pre>";
+    var_dump(array_values($data));
+    echo  "<br/>";
+    sort($data);
+    var_dump(array_values($data));
+    echo  "<br/>";
+    echo in_array(17, $data[0], false);
+    echo  "<br/>";
+    print_r(range(0, 20));
+    echo count(range(0, 20));
+    echo  "<br/>";
+    echo "======================================================================<br>";
+    // string extra learning
     $string = "loremitps doller smit";
     $stringTage = "<div class='container'><span><i>container fluid</i></span></div>";
+    echo  "<br/>";
+    echo chr('65');
+    echo  "<br/>";
+    echo chop($string, "smit");
+    echo  "<br/>";
+    echo bin2hex($string);
+    echo  "<br/>";
+    echo "======================================================================<br>";
+    // useful string test from code lab
+    // $string = "loremitps doller smit";
+    // $stringTage = "<div class='container'><span><i>container fluid</i></span></div>";
     echo strlen($string); // get length of string
     echo  "<br/>";
     echo str_word_count($string); //count how many word in a string
@@ -27,7 +79,7 @@
     echo  "<br/>";
     var_dump(strip_tags($stringTage, '<i></i>'));
     echo  "<br/>";
-    echo str_replace('doller','smit doller',$stringTage)
+    echo str_replace('doller', 'smit doller', $stringTage)
 
 
     ?> </body>
